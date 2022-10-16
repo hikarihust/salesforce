07a6f9921c2eafadde730ac782388b3ab94d98cc
vudinhquang <vudinhquangk53@gmail.com>
2022-10-16 23:42:03 +0900

    Handling Browser Events

--- Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
+    startGame: function (component, event, helper) {
+        console.log("The start new game button is clicked");
+    },
 
+    reshuffleBoard: function (component, event, helper) {
+        console.log("Reshuffle board is called");
     }
 })
