1697b34303cb9e5ea18407c64ff33e70d542116e
vudinhquang <vudinhquangk53@gmail.com>
2022-10-17 00:50:57 +0900

    Access Attributes in Javascript

--- Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -6,8 +6,11 @@
         // access the value of combobox
         let selectedValue = gameModeComboBox.get("v.value");
 
+        // update selectedMode attribute
+        component.set("v.selectedMode", selectedValue);
         console.log("The start new game button is clicked. The game mode is " + selectedValue);
-        alert("The start new game button is clicked. The game mode is " + selectedValue);
+        
+        console.log("Selected mode attribute value: " + component.get("v.selectedMode"));
     },
 
     reshuffleBoard: function (component, event, helper) {
