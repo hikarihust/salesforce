60e07f63c5b0a74710c134339f493231fafe8530
Vu Dinh Quang <quang-vu@dimage.co.jp>
2022-10-17 12:30:55 +0900

    Component Lifecycle

--- Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/Board/BoardController.js
+++ Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/Board/BoardController.js
@@ -1,5 +1,11 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit: function (component, event, helper) {
+        console.log("Initialiation completed");
+        // build a list of 100 words
+        console.log("Words: " + helper.getWords(6));
+    },
 
+    doRender: function (component, event, helper) {
+        console.log("Render completed");
     }
-})
+});
