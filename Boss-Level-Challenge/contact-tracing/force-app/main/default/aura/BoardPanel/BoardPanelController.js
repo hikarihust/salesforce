0d30cebc1bd8ec67c8224ff3e3496d8469c92fd5
vudinhquang <vudinhquangk53@gmail.com>
2022-10-18 00:45:35 +0900

    Composition and Public Attributes

--- Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -8,9 +8,6 @@
 
         // update selectedMode attribute
         component.set("v.selectedMode", selectedValue);
-        console.log("The start new game button is clicked. The game mode is " + selectedValue);
-        
-        console.log("Selected mode attribute value: " + component.get("v.selectedMode"));
     },
 
     reshuffleBoard: function (component, event, helper) {
