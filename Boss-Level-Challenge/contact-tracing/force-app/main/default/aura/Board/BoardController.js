d4bfece72fad4ebc00d36c3b66a527068eaa2616
Vu Dinh Quang <quang-vu@dimage.co.jp>
2022-10-17 16:27:35 +0900

    Loops in Aura

--- Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/Board/BoardController.js
+++ Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/Board/BoardController.js
@@ -1,14 +1,29 @@
 ({
     doInit: function (component, event, helper) {
         console.log("Initialiation completed");
+        // get game mode
+        const gameMode = component.get("v.mode");
+        let column = 0;
+        // get number of columns based on game mode
+        // double equals "1" == 1
+        // triple equals 1 === 1
+        if (gameMode && gameMode === "hard") {
+            column = 6;
+        } else if (gameMode === "medium") {
+            column = 4;
+        } else {
+            column = 3;
+        }
+
+        // get block width/size
+        let blockSize = 12 / column;
+        component.set("v.blockSize", blockSize);
         // build a list of 100 words
-        const words = helper.getWords(6);
+        const words = helper.getWords(column * column);
         component.set("v.words", words);
-        console.log("Words: " + words);
         // get win word
         const winWord = helper.getWinWord(words);
         component.set("v.winWord", winWord);
-        console.log("Win word: " + helper.getWinWord(words));
     },
 
     doRender: function (component, event, helper) {
