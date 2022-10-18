1d7a57b744f00abbe616c7ff6e5fd8e2b8d2652c
vudinhquang <vudinhquangk53@gmail.com>
2022-10-18 23:45:44 +0900

    Disable Board Component

--- Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/Board/BoardHelper.js
+++ Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/Board/BoardHelper.js
@@ -125,5 +125,13 @@
     getWinWord: function (arr) {
         const randomIndex = Math.floor(Math.random() * arr.length);
         return arr[randomIndex];
+    },
+
+    disableBoard: function (component) {
+        component.set("v.boardDisabled", true);
+    },
+
+    enableBoard: function (component) {
+        component.set("v.boardDisabled", false);
     }
 });
