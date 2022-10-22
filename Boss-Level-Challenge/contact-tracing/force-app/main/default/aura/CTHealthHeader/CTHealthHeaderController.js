da67c0dd214b1d58c07e9bf998e263794c1f844c
vudinhquang <vudinhquangk53@gmail.com>
2022-10-22 11:36:29 +0900

    CT Challenge 17 - Make Interactive With Javascript

--- Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
+++ Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
-
+    createRecord: function (component, event, helper) {
+        const createRecordEvent = $A.get("e.force:createRecord");
+        const scope = component.get("v.scope");
+        createRecordEvent.setParams({
+            entityApiName: scope === "person" ? "Person__c" : "Location__c"
+        });
+        createRecordEvent.fire();
     }
-})
+});
