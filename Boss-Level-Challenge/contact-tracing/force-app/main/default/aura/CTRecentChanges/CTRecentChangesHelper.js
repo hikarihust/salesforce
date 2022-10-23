d0986bebaab596c1a8bf032cd11691c764d7f4d0
vudinhquang <vudinhquangk53@gmail.com>
2022-10-23 20:28:04 +0900

    Datatable - Dynamic Datatable

---
+++ Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/CTRecentChanges/CTRecentChangesHelper.js
@@ -0,0 +1,15 @@
+({
+    fetchRecentHealthChanges: function (component) {
+        let action = component.get("v.scope") === "person" ? component.get("c.getRecentPersonHealthChanges") : component.get("c.getRecentLocationHealthChanges");
+
+        action.setCallback(this, function (response) {
+            const state = response.getState();
+
+            if (state === "SUCCESS") {
+                const data = response.getReturnValue();
+                component.set("v.data", data)
+            }
+        });
+        $A.enqueueAction(action);
+    }
+});
