61153d93f7468b7ebeae493405ccb494fcef2806
vudinhquang <vudinhquangk53@gmail.com>
2022-10-27 01:31:39 +0900

    Location View and Person View

--- Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
+++ Boss-Level-Challenge/contact-tracing/force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
@@ -8,7 +8,8 @@
                 { label: "Red Score", fieldName: "Red_Score__c", type: "number" },
                 { label: "Pincode", fieldName: "Pincode__c", type: "text" },
                 { label: "Address", fieldName: "Address__c", type: "text" },
-                { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" }
+                { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" },
+                { label: "View", type: "button", initialWidth: 135, typeAttributes: { label: "View/Update", name: "view_details", title: "Click to View Details" } }
             ]);
         } else {
             component.set("v.columns", [
@@ -16,7 +17,8 @@
                 { label: "Phone", fieldName: "Mobile__c", type: "text" },
                 { label: "Token", fieldName: "Token__c", type: "text" },
                 { label: "Health Status", fieldName: "Health_Status__c", type: "text" },
-                { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" }
+                { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" },
+                { label: "View", type: "button", initialWidth: 135, typeAttributes: { label: "View/Update", name: "view_details", title: "Click to View Details" } }
             ]);
         }
         helper.fetchRecentHealthChanges(component);
@@ -31,6 +33,23 @@
         if (isEnterKey) {
             component.set("v.issearching", true);
             helper.searchRecord(component, queryTerm);
+        }
+    },
+
+    handleRowAction: function (component, event, helper) {
+        const action = event.getParam("action");
+        const row = event.getParam("row");
+        const scope = component.get("v.scope");
+
+        switch (action.name) {
+            case "view_details":
+                const appEvent = scope === "person" ? $A.get("e.c:CTPersonSelectEvent") : $A.get("e.c:CTLocationSelectEvent");
+                appEvent.setParams({
+                    recordId: row.Id,
+                    status: scope === "person" ? row.Health_Status__c : row.Status__c
+                });
+                appEvent.fire();
+                break;
         }
     }
 })
